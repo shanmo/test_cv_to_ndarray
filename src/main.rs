03a4289a8bd5a2f6377ae1250b14d710c8a47d88
shanmo <sholmes9091@gmail.com>
2022-05-27 13:48:19 +0000

    add

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
 use cv_convert::{FromCv, IntoCv, TryFromCv, TryIntoCv};
 use ndarray as nd;
 use opencv as cv2;
+use opencv::prelude::*;
 
 fn main() {
     let path = "./lena.png".to_string(); 
     let img = cv2::imgproc::imread(path, cv2::imgproc::IMREAD_COLOR).unwrap(); 
     let arr = img.try_from_cv(); 
     let img_rec = arr.into_cv(); 
-    cv2::imgproc::imshow("img", &img_rec);
-    cv2::imgproc::imshow("reconstruct", &img_rec);  
+    cv2::highgui::imshow("img", &img);
+    cv2::highgui::imshow("reconstruct", &img_rec);  
     cv2::wait_key(0); 
 }
